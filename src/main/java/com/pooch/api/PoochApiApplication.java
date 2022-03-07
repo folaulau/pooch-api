9a3bb0e9e93f74f16eb15a7bc75d288df2b4d40e
Folau Kaveinga <folaukaveinga@gmail.com>
2022-03-07 10:52:12 -0700

    springboot set up

---
+++ src/main/java/com/pooch/api/PoochApiApplication.java
@@ -0,0 +1,13 @@
+package com.pooch.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoochApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoochApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pooch/api/PoochApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pooch.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoochApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
