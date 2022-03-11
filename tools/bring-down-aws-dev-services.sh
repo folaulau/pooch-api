09df6d513c09c441c7f4cc6b0232d9a71f2bab46
Folau Kaveinga <folaukaveinga@gmail.com>
2022-03-11 00:27:21 -0700

    hasura

--- tools/bring-down-aws-dev-services.sh
+++ tools/bring-down-aws-dev-services.sh
@@ -1,42 +1,42 @@
-aws ecs update-service --cluster learnmymath-api --service learnmymath-api-dev-service \
---task-definition learnmymath-api-dev-task \
+aws ecs update-service --cluster pooch-api-dev --service pooch-api-dev \
+--task-definition pooch-api-dev \
 --desired-count 0 \
---profile folauk110 \
+--profile pooch \
 --output text \
 --no-cli-pager
 
 read -t 1 -p "taking down api server."
 
-aws ecs update-service --cluster learnmymath-api --service learnmymath-graphql-dev-service \
---task-definition learnmymath-graphql-dev \
+aws ecs update-service --cluster pooch-api-dev --service pooch-graphql-dev \
+--task-definition pooch-graphql-dev \
 --desired-count 0 \
---profile folauk110 \
+--profile pooch \
 --output text \
 --no-cli-pager
 
 read -t 1 -p "taking down graphql server."
 
-aws rds stop-db-instance --profile folauk110 \
---db-instance-identifier learnmymath-api-dev \
+aws rds stop-db-instance --profile pooch \
+--db-instance-identifier pooch-api-dev-db \
 --output text \
 --no-cli-pager
 
 read -t 1 -p "taking down postgres server."
 
 echo "rename index.html to old-index.html"
-aws s3 mv s3://dev.learnmymath.io/index.html s3://dev.learnmymath.io/old-index.html \
---profile folauk110 \
+aws s3 mv s3://dev.poochapp.com/index.html s3://dev.poochapp.com/old-index.html \
+--profile pooch \
 --output text \
 --no-cli-pager
 echo "index.html has been renamed to old-index.html"
 
 read -t 3 -p "uploading maintenance index file"
 
-aws s3 cp index.html s3://dev.learnmymath.io/index.html \
+aws s3 cp index.html s3://dev.poochapp.com/index.html \
 --content-type 'text/html' \
 --cache-control 'no-store, max-age=0' \
 --metadata-directive REPLACE \
---profile folauk110 \
+--profile pooch \
 --output text \
 --no-cli-pager
 echo "maintenance index file has been uploaded to s3"
@@ -45,7 +45,7 @@ read -t 3 -p "invalidating cloudfront"
 
 aws cloudfront create-invalidation --distribution-id=E3FP06IF5DA4AS \
 --paths '/index.html' '/version.txt' \
---profile folauk110 \
+--profile pooch \
 --output text \
 --no-cli-pager
 echo "cloudfront restarted"
--- tools/bring-up-aws-dev-services.sh
+++ tools/bring-up-aws-dev-services.sh
@@ -1,24 +1,24 @@
 aws rds start-db-instance \
---profile folauk110 \
---db-instance-identifier learnmymath-api-dev \
+--profile pooch \
+--db-instance-identifier pooch-api-dev-db \
 --output text \
 --no-cli-pager
 
 read -t 180 -p "postgres is starting up. waiting for 3 mins before starting api server."
 
-aws ecs update-service --cluster learnmymath-api --service learnmymath-api-dev-service \
---task-definition learnmymath-api-dev-task \
+aws ecs update-service --cluster pooch-dev --service pooch-api-dev \
+--task-definition pooch-api-dev \
 --desired-count 1 \
---profile folauk110 \
+--profile pooch \
 --output text \
 --no-cli-pager
 
 read -t 0 -p "api server is starting up. starting graphql server..."
 
-aws ecs update-service --cluster learnmymath-api --service learnmymath-graphql-dev-service \
---task-definition learnmymath-graphql-dev \
+aws ecs update-service --cluster pooch-api --service pooch-graphql-dev \
+--task-definition pooch-graphql-dev \
 --desired-count 1 \
---profile folauk110 \
+--profile pooch \
 --output text \
 --no-cli-pager
 
@@ -26,16 +26,16 @@ aws ecs update-service --cluster learnmymath-api --service learnmymath-graphql-d
 read -t 90 -p "graphql server is starting up."
 
 echo "removing maintenance index file"
-aws s3 rm s3://dev.learnmymath.io/index.html \
---profile folauk110 \
+aws s3 rm s3://dev.poochapp.net/index.html \
+--profile pooch \
 --output text \
 --no-cli-pager
 echo "maintenance index file has been removed"
 
 read -t 3 -p "renaming old-index.html back to index.html"
 
-aws s3 mv s3://dev.learnmymath.io/old-index.html s3://dev.learnmymath.io/index.html \
---profile folauk110 \
+aws s3 mv s3://dev.poochapp.com/old-index.html s3://dev.poochapp.com/index.html \
+--profile pooch \
 --output text \
 --no-cli-pager
 echo "old-index.html has been renamed back to index.html"
@@ -44,7 +44,7 @@ read -t 3 -p "invalidating cloudfront"
 
 aws cloudfront create-invalidation --distribution-id=E3FP06IF5DA4AS \
 --paths '/index.html' '/version.txt' \
---profile folauk110 \
+--profile pooch \
 --output text \
 --no-cli-pager
 echo "cloudfront restarted"
