6e988def6e506ccee7c892292e7bbef488309021
Folau Kaveinga <folaukaveinga@gmail.com>
2022-03-10 18:59:50 -0700

    added ping controller

--- src/main/java/com/pooch/api/entity/petsitter/PetSitterRestController.java
+++ src/main/java/com/pooch/api/entity/petsitter/PetSitterRestController.java
@@ -1,5 +1,29 @@
 package com.pooch.api.entity.petsitter;
 
+import static org.springframework.http.HttpStatus.OK;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+
+@Tag(name = "PetSitters", description = "PetSitter Operations")
+@Slf4j
+@RestController
+@RequestMapping("/petsitters")
 public class PetSitterRestController {
+
+    @Operation(summary = "Sign Up", description = "sign up")
+    @PostMapping(value = "/signup")
+    public ResponseEntity<String> signUp(@RequestHeader(name = "x-api-key", required = true) String xApiKey) {
+        log.info("sign up");
+
+        return new ResponseEntity<>("Looks Good", OK);
+    }
 
 }
