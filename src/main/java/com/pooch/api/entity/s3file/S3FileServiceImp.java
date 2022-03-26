4f7e124160a7b64046a8e16c18835e2693051402
Folau Kaveinga <folaukaveinga@gmail.com>
2022-03-26 16:39:25 -0600

    Update S3FileServiceImp.java

--- src/main/java/com/pooch/api/entity/s3file/S3FileServiceImp.java
+++ src/main/java/com/pooch/api/entity/s3file/S3FileServiceImp.java
@@ -3,6 +3,8 @@ package com.pooch.api.entity.s3file;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.pooch.api.dto.S3FileDTO;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Service
@@ -12,5 +14,10 @@ public class S3FileServiceImp implements S3FileService {
     @Autowired
     private S3FileDAO s3FileDAO;
 
-    
+    @Override
+    public S3FileDTO refreshTTL(String uuid) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
 }
