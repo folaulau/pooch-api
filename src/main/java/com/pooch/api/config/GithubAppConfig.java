d9313b1738b3de9725d991da8ae14d912f387ff3
Folau Kaveinga <folaukaveinga@gmail.com>
2022-03-30 17:41:57 -0600

    fixed github elasticsearch

--- src/main/java/com/pooch/api/config/GithubAppConfig.java
+++ src/main/java/com/pooch/api/config/GithubAppConfig.java
@@ -55,7 +55,7 @@ import org.springframework.data.elasticsearch.core.mapping.SimpleElasticsearchMa
 @Slf4j
 @Profile({"github"})
 @Configuration
-// @PropertySource("classpath:config/application-github.properties")
+@PropertySource("classpath:config/application-github.properties")
 public class GithubAppConfig {
 
     @Value("${aws.deploy.region:us-west-2}")
